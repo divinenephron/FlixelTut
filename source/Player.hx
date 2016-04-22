23806efb6acdd3a8c662ce47fcc6d7a10e7fbd06
Devon Buchanan <devon@divinenephron.co.uk>
2016-04-22 15:35:19 +0100

    Adding sprite animation to player.

--- source/Player.hx
+++ source/Player.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.math.FlxPoint;
@@ -10,7 +11,12 @@ class Player extends FlxSprite {
       public var speed:Float = 200;
       public function new(?X:Float=0, ?Y:Float=0){
              super(X, Y);
-             makeGraphic(16, 16, FlxColor.BLUE);
+             loadGraphic(AssetPaths.player__png, true, 16, 16);
+             setFacingFlip(FlxObject.LEFT, false, false);
+             setFacingFlip(FlxObject.RIGHT, true, false);
+             animation.add("lr", [3, 4, 3, 5], 6, false);
+             animation.add("u", [6, 7, 6, 8], 6, false);
+             animation.add("d", [0, 1, 0, 2], 6, false);
              drag.x = drag.y = 1600;
       }
       private function movement():Void {
@@ -40,6 +46,7 @@ class Player extends FlxSprite {
                     } else if (_right) {
                       mA += 45;
                     }
+                    facing = FlxObject.UP;
                  } else if (_down) {
                    mA = 90;
                    if (_left) {
@@ -47,15 +54,30 @@ class Player extends FlxSprite {
                    } else if (_right) {
                      mA -= 45;
                    }
+                   facing = FlxObject.DOWN;
                  } else if (_left) {
                    mA = 180;
+                   facing = FlxObject.LEFT;
                  } else if (_right) {
                    mA = 0;
+                   facing = FlxObject.RIGHT;
                  }
                  velocity.set(speed, 0);
                  velocity.rotate(FlxPoint.weak(0, 0), mA);
+
+                 if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE) {
+                    switch (facing) {
+                        case FlxObject.LEFT, FlxObject.RIGHT:
+                             animation.play("lr");
+                        case FlxObject.UP:
+                             animation.play("u");
+                        case FlxObject.DOWN:
+                             animation.play("d");
+                    }
+                 }
               }
       }
+      
       override public function update(elapsed:Float):Void {
                movement();
                super.update(elapsed);
