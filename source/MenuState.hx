a6d73338743f975f67ecfccafa58e3ce8e49963d
Devon Buchanan <devon@divinenephron.co.uk>
2016-04-30 09:38:47 +0100

    Adding options screen.

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -10,12 +10,25 @@ import flixel.math.FlxMath;
 class MenuState extends FlxState
 {
 	private var _btnPlay:FlxButton;
+	private var _txtTitle:FlxText;
+	private var _btnOptions:FlxButton;
 
 	override public function create():Void
 	{
+		_txtTitle = new FlxText(20, 0, 0, "HaxeFlixel\nTutorial\nGame", 22);
+		_txtTitle.alignment = CENTER;
+		_txtTitle.screenCenter(X);
+		add(_txtTitle);
+		
 		_btnPlay = new FlxButton(0,0,"Play", clickPlay);
 		_btnPlay.screenCenter();
 		add(_btnPlay);
+		
+		_btnOptions = new FlxButton(0, 0, "Options", clickOptions);
+		_btnOptions.x = (FlxG.width / 2) + 10;
+		_btnOptions.y = FlxG.height - _btnOptions.height - 10;
+		add(_btnOptions);
+		
 		super.create();
 	}
 
@@ -27,5 +40,10 @@ class MenuState extends FlxState
 	private function clickPlay():Void
 	{
 		FlxG.switchState(new PlayState());
+	}
+	
+	private function clickOptions():Void
+	{
+		FlxG.switchState(new OptionsState());
 	}
 }
